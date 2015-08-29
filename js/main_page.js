c69578495c98b283259db56c7580d93afdfadd33
Chen Di <sundychenya1018@yahoo.com>
2015-08-29 18:57:23 +0800

    Add special effect panel

--- js/main_page.js
+++ js/main_page.js
@@ -170,3 +170,22 @@ function change_background() {
 		update_bg(new_bg_clear, new_clear_path);
 	}
 }
+
+// Find the right method, call on correct element
+function launchIntoFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
+// Launch fullscreen for browsers that support it
+function wholePageFullScreen() {
+	launchIntoFullscreen(document.documentElement);
+}
+
