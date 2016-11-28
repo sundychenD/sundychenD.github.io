0fe573e4c1bf4e38b469413614f8146d6860b7d6
Chen Di <sundychenya1018@yahoo.com>
2016-11-29 03:16:29 +0800

    Billsplitt milestone 1

---
+++ demos/billsplitter/js/animate.js
@@ -0,0 +1,29 @@
+$(window).scroll(function () {
+	var scrollPos = $(document).scrollTop();
+	var problemTriggerPoint = $('#problem').offset().top - 300;
+	var solution_1_TriggerPoint = $('#solution-1').offset().top - 300;
+	var solution_2_TriggerPoint = $('#solution-2').offset().top - 300;
+	var solution_3_TriggerPoint = $('#solution-3').offset().top - 300;
+
+	if (scrollPos > problemTriggerPoint) {
+		['#problem-paragraph-1', '#problem-paragraph-2', '#problem-paragraph-3'].forEach(addFadeInUpAnimation)
+	}
+
+	if (scrollPos > solution_1_TriggerPoint) {
+		['.solution-1-img'].forEach(addFadeInUpAnimation)
+	}
+
+	if (scrollPos > solution_2_TriggerPoint) {
+		['.solution-2-img'].forEach(addFadeInUpAnimation)
+	}
+
+	if (scrollPos > solution_3_TriggerPoint) {
+		['.solution-3-img'].forEach(addFadeInUpAnimation)
+	}
+});
+
+function addFadeInUpAnimation(element, index, array) {
+	$(element).removeClass('transparent')
+	$(element).addClass('animated');
+	$(element).addClass('fadeInUp');
+}
